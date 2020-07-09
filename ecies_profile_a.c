95b7b3f8eca6b4a806a672c5d73b822a87a241a3
oatil <oatil@foxmail.com>
2020-07-09 15:20:19 +0800

    Update ecies_profile_a.c

--- ecies_profile_a.c
+++ ecies_profile_a.c
@@ -1,3 +1,21 @@
+/**
+The ME and SIDF shall implement this profile.The ECIES parameters for this profile shall be the following :
+- EC domain parameters : Curve25519[46]
+- EC Diffie - Hellman primitive : X25519[46]
+- point compression : N / A
+- KDF : ANSI - X9.63 - KDF[29]
+- Hash : SHA - 256
+- SharedInfo1 : (the ephemeral public key octet string – see[29] section 5.1.3)
+- MAC: HMAC–SHA - 256
+- mackeylen : 32 octets(256 bits)
+- maclen : 8 octets(64 bits)
+- SharedInfo2 : the empty string
+- ENC : AES–128 in CTR mode
+- enckeylen : 16 octets(128 bits)
+- icblen : 16 octets(128 bits)
+- backwards compatibility mode : false
+**/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -8,6 +26,35 @@
 #include <openssl/rand.h>
 #include <openssl/evp.h>
 
+int encrypt(uint8_t* msg,
+	size_t          msg_len,
+	int             curve,
+	const uint8_t* peer_pubk,
+	const uint8_t   peer_pubk_len,
+	uint8_t** epubk,          // out (must free)
+	size_t* epubk_len,      // out
+	uint8_t** iv,             // out (must free)
+	uint8_t* iv_len,         // out
+	uint8_t** tag,            // out (must free)
+	uint8_t* tag_len,        // out
+	uint8_t** ciphertext,     // out (must free)
+	uint8_t* ciphertext_len) // out
+{
+        return 0;
+}
+
+int decrypt(const EC_KEY* ec_key,
+	const uint8_t* peer_pubk,
+	const uint8_t  peer_pubk_len,
+	uint8_t* iv,
+	uint32_t       iv_len,
+	uint8_t* tag,
+	uint32_t       tag_len,
+	uint8_t* ciphertext,
+	uint32_t       ciphertext_len)
+{
+        return 0;
+}
 int main(int argc, char * argv[])
 {
         return 0;
